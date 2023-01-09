f88481287d7909a017f3496d6f43a07e8d2b3e1d
GraceThree <grace3unger@gmail.com>
2023-01-08 20:03:31 -0800

    Initial commit 1/8/23

--- tests.py
+++ tests.py
@@ -1,15 +1,5 @@
-# contains current tests for CA system
+import CA as ca
 
-
-x = ca.Symbol("x")
-y = ca.Symbol("y")
-z = ca.Symbol("z")
-
-print(x+y)
-print(x-y)
-
-exp1 = ca.Symbol("(2*4)+6")
-print(exp1)
-print(ca.par(x+y)*exp1)
+print(ca.eval(Symbol("(x+y)*2")))
 
 
