0b82d1d9b1131e133168fef2a3e03123cf147dca
GraceThree <grace3unger@gmail.com>
2023-04-25 00:03:15 -0700

    modified:   CA.py

--- CA.py
+++ CA.py
@@ -1,30 +1,63 @@
-# Gracious Computer-Algebra
+# Graciously Computer-Algebra
 # Author: Grace Unger
 # Created: 1-8-23
-# Modified: 1-8-23
+# Modified: 4-24-23
 # Stores an input string as a list of tokens
 # and evaluates simple expressions according to a Shunting Yard algorithm
-#
-#
 
 import math
+import re
+
+# Constants: 
+#   OPERATORS: Symbol:Precendence
+#   RESERVED_SYMBOLS: Disallowed names of variables
+#   OPERATOR_FUNCTIONS: Anonymous functions for each predefined operator
+#   OPERATOR_ARGUMENTS: Number of inputs for each predefined operator
 class Symbol:
 
-    operators = {"*":3, "-":2, "+":2, "/":3, "^":4,
+    OPERATORS = {"*":3, "-":2, "+":2, "/":3, "^":4,
                  "sin":5, "cos":5, "tan":5, "log":5}
-    reserved=("*", "-", "+", "/", "sin", "cos", "tan", "(", ")")
-    opRules={"*": lambda x,y: x*y, "+": lambda x,y: x+y,
-             "-": lambda x,y:x-y, "/": lambda x,y: x/y,
-             "sin": lambda x: math.sin(x), "cos": lambda x:math.cos(x),
-             "tan": lambda x :math.tan(x), "log": lambda x:math.log(x)}
-    opArgs = {"*":2, "-":2, "+":2, "/":2, "^":2,
+    RESERVED_SYMBOLS=("*", "-", "+", "/", "sin", "cos", "tan", "(", ")")
+    OPERATOR_FUNCTIONS={"*": lambda x, y: x * y, 
+             "+": lambda x, y: x + y,
+             "-": lambda x , y: x - y,
+             "/": lambda x , y: x / y,
+             "sin": lambda x: math.sin(x),
+             "cos": lambda x:math.cos(x),
+             "tan": lambda x :math.tan(x), 
+             "log": lambda x:math.log(x)}
+    OPERATOR_VALENCE = {"*":2, "-":2, "+":2, "/":2, "^":2,
               "sin":1, "cos":1, "tan":1, "log":1}
 
-    def __init__(self, tokens):
-        self.tokens = []
-        for i in tokens:
-            self.tokens.append(i)
 
+    # Takes an input string and tokenizes it into an array of formal symbols and numbers
+    # First checks to see if the start of the string is a reserved symbol, which are taken as chunks separately
+    # Then checks if it is an alpha character and adds as a single character variable
+    # Then takes any chunk of digits and adds them as a single token
+
+    def __init__(self, inputStr = "", inputTokens = []):
+        if(inputTokens):
+            self.tokens = inputTokens
+            return
+        self.tokens = []
+        inputStr = re.sub(r" ", "", inputStr)
+        initialNumRegex = r"^[0-9]*"
+        while(inputStr):
+            initialSeg = filter(lambda x:inputStr.startsWith(x), self.RESERVED_SYMBOLS)[0]
+            if(initialSeg):
+                self.tokens += [initialSeg]
+                re.sub(rf"^{initialSeg}", "", inputStr)
+            elif(re.test(r"^[a-zA-Z]", inputStr[0])):
+                self.tokens += inputStr[0]
+                inputStr = inputStr[1:]
+            elif(re.match(initialNumRegex, inputStr)):
+                self.tokens += re.match(initialNumRegex, inputStr)
+
+            
+
+    # Defining formal operations on Symbols. 
+    # TODO: Refactor this to not require reassigning into a string
+    
     def __str__(self):
         return ''.join(x for x in self.tokens)
 
@@ -36,54 +69,18 @@ class Symbol:
 
     def __mul__(self, factor):
         return Symbol(f"{self}*{factor}")
+    
+    def __truediv__(self, divisor):
+        return Symbol(f"{self}/{divisor}")
+    
+    def __neg__(self):
+        return Symbol(f"-{self}")
 
-    @staticmethod
     def par(exp):
         return Symbol(f"({exp})")
+    
 
-
-    # Symbol -> Symbol
-    # Uses Shunting Yard algorithm to convert input to RPN output, reads output to evaluate.
-    # Numerical values are properly evaluated and symbolic values are converted back, into symbols
-    @staticmethod
-    def simplify(exp):
-        out = []
-        op = []
-        for i in exp.tokens:
-            if not i in Symbol.reserved: out += i+" "
-            if i in op:
-                while Symbol.operators[op[-1]]>Symbol.operators[i]:
-                    out.append(op.pop())
-                op.append(i)
-            if i == "(": op.append("(")
-            if i == ")":
-                while not op[-1] =="(":out.append(op.pop())
-                out.append(op.pop())
-        while op:out.append(op.pop())
-        temp = []
-        # TODO: Figure out how to track when this is done or not.
-        # Do we want to just check when we run out of operators in the stack?
-        while out:
-            temp.append(out[0])
-            out = out[1:]
-            if temp[-1] in Symbol.operators:
-                opr = temp[-1]
-                if Symbol.opArgs[opr] == 1:
-                    if temp[-2]:
-                        if temp[-2].isdigit(): out = [Symbol.opRules[opr](temp[-2]) + ''] + out
-                        else: out = [Symbol(f"{opr}({temp[-2]})")]
-                    else:
-                        print(f"Operator {opr} has insufficient arguments")
-                        return
-                if Symbol.opArgs[opr] == 2:
-                    if not temp[-2] or not temp[-3]:
-                        print(f"Operator {opr} has insufficient arguments")
-                        return
-                    if temp[-2].isdigit() and temp[-3].isdigit(): out = [Symbol.opRules[opr](temp[-3], temp[-2])+'']+out
-                    elif opr == "*":
-                        if temp[-2].isdigit(): out = [f"{temp[-2]}{temp[-3]}"] + out
-                        else: out = [f"{temp[-3]}{temp[-2]}"] + out
-                    if opr in ["+", "-"]: op[0] = op[f"({op[0]})"]
-
-
-
+    # Converts a linear arithmetic symbol into Reverse-Polish Notation
+    # According to the Shunting-Yard Algorithm
+    def linearToRPN(self):
+        while(self.tokens):
