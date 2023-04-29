3b15254628ddf7045c64f250cc0d75023077bd1c
GraceThree <grace3unger@gmail.com>
2023-04-28 17:31:49 -0700

    modified:   tests.py 	modified:   Expression.py

--- tests.py
+++ tests.py
@@ -3,17 +3,18 @@ from Expression import Expression
 # Unit tests for numerical expression inputs
 # Currently: Tests RPN conversion and evaluation separately 
 # Note: Requires 1 trailing space in test strings. 
+
+#
 def testNums():
-    numTest = Expression("(11+1)^2 - 4")
-    numTestTwo = Expression("(11+1)^2 - 4")
+    numTest = Expression("(11+1)^2 - x")
+    numTestTwo = Expression("(11+1)^2 - x")
 
     numTest.process()
-    assert str(numTest) == "11 1 + 2 ^ 4 - "
+    assert str(numTest) == "11 1 + 2 ^ x - "
 
     numTestTwo.evaluate()
     print(f"numTestTwo evaluates to {str(numTestTwo)}")
-    assert str(numTestTwo) == "140 "
-
+    assert str(numTestTwo) == "144 x - "
 
 # Unit tests for basic symbolic inputs with no numeric simplification needed
 # Currently: Tests processing into RPN and that evaluation doesn't change anything
@@ -27,5 +28,5 @@ def testSymb():
     varTestTwo.evaluate()
     assert str(varTestTwo) == "x y + 2 ^ 3 "
 
-testNums()
-#testSymb()
+#testNums()
+testSymb()
